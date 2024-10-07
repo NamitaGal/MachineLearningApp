93115c3e879f9c0e0c8e4a6284001d3e44cae07c
NamitaGal <namitha.galtechschool@gmail.com>
2024-10-07 11:00:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ with st.expander("data"):
   y
   
 with st.expander("visualization"):
-  st.scatter_chart(data=df, x="bill_length_mm",y="body_mass_g",colors="species")
+  st.scatter_chart(data=df, x="bill_length_mm",y="body_mass_g",color="species")
