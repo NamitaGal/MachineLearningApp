c033f4a656eb6a58735ae14e479026801fe23c31
NamitaGal <namitha.galtechschool@gmail.com>
2024-10-07 10:40:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App')
 
 st.info('this is an app bulids a machine learning model')
+
+df = pd.read_csv("https://www.kaggle.com/code/rv1922/penguins-analysis-eda/input?scriptVersionId=191134290")
+df
