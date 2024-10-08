47a535a4a7604449f93005ca1254c6f3f6957696
NamitaGal <namitha.galtechschool@gmail.com>
2024-10-08 10:16:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.info('this is an app bulids a machine learning model')
 
 with st.expander("data"):
   st.write('**dataset**')
-  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+  df = pd.read_csv("https://raw.githubusercontent.com/NamitaGal/MachineLearningApp/refs/heads/master/penguins_cleaned.csv")
   df
   st.write('**x**')
   x_raw = df.drop('species',axis=1)
